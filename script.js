979a27440b32b1d4d092e3a124bb950d8b698423
ycahnn <104204689+ycahnn@users.noreply.github.com>
2023-10-15 03:18:54 +0900

    typing effect

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-document.getElementById('contact-form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert('Form submitted!'); // Add your own logic here
+var text = "Hello, world!"
+var outputElement = document.getElementById('text');
+var cursorElement = document.getElementById('cursor');
+
+function typeWriter(text, i) {
+    if (i < text.length) {
+        outputElement.innerHTML += text.charAt(i);
+        i++;
+        setTimeout(function() {
+            typeWriter(text, i);
+        }, 150);
+    } 
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    typeWriter(text, 0);
 });
